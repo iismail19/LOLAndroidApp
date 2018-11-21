a808c79b1a25d26fc339cd1827ec9a898f8e87f3
iismail1997 <2015.ismail@gmail.com>
2018-11-21 00:42:42 -0600

    not building images need to be lowerCaseName

--- app/src/main/java/com/comp/lolandroidapp/MainActivity.java
+++ app/src/main/java/com/comp/lolandroidapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.comp.lolandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView summonerName = findViewById(R.id.summonerNameText);
+        Button submit = findViewById(R.id.submitButton);
+        String sName = summonerName.getText().toString();
+
     }
 }
