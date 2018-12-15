da3c9cea666ab72ece28eda7d0d281cacd11aa92
plempke <31676433+plempke@users.noreply.github.com>
2018-12-15 11:32:51 -0600

    remade the xml doc with class
    Signed-off-by: plempke <31676433+plempke@users.noreply.github.com>

---
+++ app/src/main/java/com/comp/lolandroidapp/summoner_returned.java
@@ -0,0 +1,13 @@
+package com.comp.lolandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class summoner_returned extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summoner_returned);
+    }
+}
